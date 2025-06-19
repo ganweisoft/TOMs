4e7205cf6853aca08e77f2ebdced42deb9fa30a1
wangpeng <wangpeng@ganweisoft.com>
2025-06-19 22:26:28 +0800

    feat：update TOMs.build.bat

--- TOMs.build.bat
+++ TOMs.build.bat
@@ -2,21 +2,25 @@
 
 cd /d "%~dp0%"
 
-@echo off
+chcp 65001 >nul
 
 rem Unified script for both initial setup and updates
 rem Check if repository exists
-if not exist "TOMs\.git" (
-    echo Initial clone required...
+dir /ad ".git" >nul 2>&1
+if %errorlevel% == 0 (
+	echo Repository exists, pulling updates...
+    git pull
+) else (
+	echo Initial clone required...
     git clone https://github.com/ganweisoft/TOMs.git
     cd TOMs
-) else (
-    echo Repository exists, pulling updates...
-    cd TOMs
-    git pull
 )
-
 rem Common operations for both cases
 git submodule init
 git submodule update --remote
+if not exist "build.bat" (
+    echo build.bat not exist
+	pause
+	exit /b
+)
 call build.bat
