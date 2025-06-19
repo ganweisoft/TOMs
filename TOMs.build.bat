3975ffeef39aff6af5c8c9b9707c5277db97bab7
wangpeng <wangpeng@ganweisoft.com>
2025-06-19 14:05:03 +0800

    feat：update TOMs.build.bat

---
+++ TOMs.build.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+
+rem =============If this is the first time cloning the repository for building, execute the following code============= 
+git clone https://github.com/ganweisoft/TOMs.git
+cd TOMs
+git submodule init
+git submodule update
+build.bat
+
+rem ============= Otherwise, execute the following code=============
+rem git pull
+rem git submodule update --remote
+rem build.bat
