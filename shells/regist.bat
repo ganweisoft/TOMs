e158fef04269b93d5d927c295ff1002dc3ac0e7b
wangpeng <wangpeng@ganweisoft.com>
2025-06-23 18:47:39 +0800

    feat：update shell

--- shells/regist.bat
+++ shells/regist.bat
@@ -1,16 +1,38 @@
 @echo off
+setlocal enabledelayedexpansion
 
-rem ÂàáÊç¢Âà∞ÂΩìÂâçÊâπÂ§ÑÁêÜÊñá‰ª∂ÊâÄÂú®ÁõÆÂΩï
+title Register TOMs Windows Service
+	
 cd /d "%~dp0%"
 
-chcp 65001 >nul
+rem …Ë÷√◊¢≤·»’÷æƒø¬º
+set "logs_dir=%cd%\regist_logs.txt"
 
-rem ËÆæÁΩÆÊûÑÂª∫Êó•ÂøóÁõÆÂΩï
-set "logs_dir=regist_logs.txt"
+:: ºÏ≤‚π‹¿Ì‘±»®œﬁ
+fltmc >nul 2>&1 || (
+	call :GetTimestamp
+    echo [!formatted_datetime!] Please right-click on this batch file and select "Run as administrator." >>%logs_dir%
+    pause
+    exit /b
+)
 
-rem Ê£ÄÊü•ÊòØÂê¶Â∑≤ÂÆâË£Ö.NET SDK
+if exist "%logs_dir%" (
+	call :GetTimestamp
+    echo [!formatted_datetime!] Attempting to delete registry log file >>%logs_dir%
+	del /q "%logs_dir%"
+	if %errorlevel% equ 0 (
+		call :GetTimestamp
+		echo [!formatted_datetime!] Successfully deleted registry log files >>%logs_dir%
+    ) else (
+		call :GetTimestamp
+		echo [!formatted_datetime!] Failed to delete registry log files^£®files might be locked or you lack proper access rights^£© >>%logs_dir%
+    )
+)
+
+
+rem ºÏ≤È «∑Ò“—∞≤◊∞.NET SDK
 call :GetTimestamp
-echo Ê≠£Âú®Ê£ÄÊµãÊòØÂê¶Â∑≤ÂÆâË£Ö.NET SDK... >>%logs_dir%
+echo [!formatted_datetime!] Checking if .NET SDK is already installed... >>%logs_dir%
 for /f "tokens=*" %%a in ('dotnet --list-sdks ^| sort /r') do (
     for /f "tokens=2 delims=[]" %%b in ("%%a") do (
         set "SDK_PATH=%%b"
@@ -19,85 +41,150 @@ for /f "tokens=*" %%a in ('dotnet --list-sdks ^| sort /r') do (
 )
 :done
 if not defined SDK_PATH (
-	echo Â∑≤Ê£ÄÊµãÂà∞.NET SDKÊú™ÂÆâË£ÖÊàñÂÆâË£ÖË∑ØÂæÑÊú™Âú®ÂÖ®Â±ÄÁéØÂ¢ÉÂèòÈáè‰∏≠ÔºåËØ∑Á°ÆËÆ§... >>%logs_dir%
+	call :GetTimestamp
+	echo [!formatted_datetime!] It has been detected that the .NET SDK is either not installed or its installation path is not included in the global environment variables. Please verify... >>%logs_dir%
     pause
     exit /b 1
 )
 if "%SDK_PATH%" == "" (
-	echo Â∑≤Ê£ÄÊµãÂà∞.NET SDKÊú™ÂÆâË£ÖÊàñÂÆâË£ÖË∑ØÂæÑÊú™Âú®ÂÖ®Â±ÄÁéØÂ¢ÉÂèòÈáè‰∏≠ÔºåËØ∑Á°ÆËÆ§... >>%logs_dir%
+	call :GetTimestamp
+	echo [!formatted_datetime!] The SDK installation path is not configured in the global environment variables... >>%logs_dir%
     pause
     exit /b 1
 )
 if exist "%SDK_PATH%\" (
-     echo .NET SDKË∑ØÂæÑ"%SDK_PATH%"Â∑≤ÂÆâË£Ö„ÄÇ>>%logs_dir%
+	 call :GetTimestamp
+     echo [!formatted_datetime!] .NET SDK found at location: "%SDK_PATH%"°£>>%logs_dir%
 ) else (
-	echo Â∑≤Ê£ÄÊµãÂà∞.NET SDKÊú™ÂÆâË£ÖÊàñÂÆâË£ÖË∑ØÂæÑÊú™Âú®ÂÖ®Â±ÄÁéØÂ¢ÉÂèòÈáè‰∏≠ÔºåËØ∑Á°ÆËÆ§... >>%logs_dir%
+	 call :GetTimestamp
+	echo [!formatted_datetime!] The installed .NET SDK path is not properly registered in the system's global environment variables... >>%logs_dir%
     pause
     exit /b 1
 )
-rem Ê£ÄÊü•ÊòØÂê¶Â∑≤ÂÆâË£Ö.NET SDK 9.0ÁâàÊú¨
+rem ºÏ≤È «∑Ò“—∞≤◊∞.NET SDK 9.0∞Ê±æ
 for /f "tokens=*" %%a in ('dotnet --version') do set SDK_VERSION=%%a
 call :GetTimestamp
-echo [!formatted_datetime!] ÂΩìÂâç.NET SDK ÁâàÊú¨: %SDK_VERSION% >>%logs_dir%
+echo [!formatted_datetime!] Current .NET SDK version: %SDK_VERSION% >>%logs_dir%
 echo %SDK_VERSION% | findstr "^9" >nul
 if %errorlevel% neq 0 (
-    echo Ë¶ÅÊ±ÇÂÆâË£Ö.NET SDK‰∏∫9.0ÁâàÊú¨
-	echo Ë¶ÅÊ±ÇÂÆâË£Ö.NET SDK‰∏∫9.0ÁâàÊú¨ >>%logs_dir%
+    call :GetTimestamp
+	echo [!formatted_datetime!] Require installation of .NET SDK version 9.0 >>%logs_dir%
 	pause
     exit /b 1
 )
-rem Ê£ÄÊü•ÊòØdotnetÂëΩ‰ª§ÊòØÂê¶Â∑≤Âä†ÂÖ•Âà∞ÁéØÂ¢ÉÂèòÈáèpath‰∏≠
+rem ºÏ≤È «dotnet√¸¡Ó «∑Ò“—º”»ÎµΩª∑æ≥±‰¡øpath÷–
 for %%A in ("%SDK_PATH%") do set "DOTNET_PATH=%%~dpA"
-rem ÂéªÈô§Êú´Â∞æÁöÑÂèçÊñúÊù†
+rem »•≥˝ƒ©Œ≤µƒ∑¥–±∏‹
 set "DOTNET_PATH=%DOTNET_PATH:~0,-1%"
-echo DOTNET SDK CLIÁõÆÂΩïÔºö%DOTNET_PATH% >>%logs_dir%
-rem ÈÅçÂéÜÁéØÂ¢ÉÂèòÈáèPath‰∏≠ÁöÑÊâÄÊúâË∑ØÂæÑ
+call :GetTimestamp
+echo [!formatted_datetime!] DOTNET SDK CLI Dicrectory£∫%DOTNET_PATH% >>%logs_dir%
+rem ±È¿˙ª∑æ≥±‰¡øPath÷–µƒÀ˘”–¬∑æ∂
 for %%P in ("%PATH:;=" "%") do (
 	set "p=%%~P"
-    rem ÂéªÈô§Êú´Â∞æÁöÑÂèçÊñúÊù†ÔºàÂ¶ÇÊûúÊúâÁöÑËØùÔºâ
+    rem »•≥˝ƒ©Œ≤µƒ∑¥–±∏‹£®»Áπ˚”–µƒª∞£©
     if "!p:~-1!"=="\" set "p=!p:~0,-1!"
     if /i "!p!"=="!DOTNET_PATH!" (
         goto DOTNET_PATH_Found
     )
 )
 :DOTNET_PATH_NOT_Found
-echo ‚ùå Ë∑ØÂæÑ "!DOTNET_PATH!" Êú™Âú®ÁéØÂ¢ÉÂèòÈáèPath‰∏≠ÊâæÂà∞ÔºåËØ∑Â∞ÜËØ•Ë∑ØÂæÑÊ∑ªÂä†Ëá≥ÁéØÂ¢ÉÂèòÈáèPath‰∏≠ÂÜçÊ¨°ÈáçËØï„ÄÇ>> "%logs_dir%"
-rem DOTNET SDK CLIÁõÆÂΩïÊú™ÊâæÂà∞Áõ¥Êé•ÈÄÄÂá∫
+call :GetTimestamp
+echo [!formatted_datetime!] Path "!DOTNET_PATH!" Not found in the environment variable Path. Please add the path to the environment variable Path and try again°£>> "%logs_dir%"
+rem DOTNET SDK CLIƒø¬ºŒ¥’“µΩ÷±Ω”ÕÀ≥ˆ
 pause
 exit /b
 :DOTNET_PATH_Found
-echo  ‚úÖ DOTNET SDK CLIË∑ØÂæÑ "%DOTNET_PATH%" Â∑≤Â≠òÂú®‰∫éÁéØÂ¢ÉÂèòÈáèPath‰∏≠„ÄÇ>> "%logs_dir%"
+call :GetTimestamp
+echo [!formatted_datetime!] DOTNET SDK CLI Path "%DOTNET_PATH%" already exists in the environment variable Path°£>> "%logs_dir%"
 
-echo Start Install IoTCenter Service
+rem sc query IoTCenterDaemon |findstr /i "STATE">nul
+rem if not errorlevel 1 (
+rem 	goto startDaemon
+rem ) else (
+rem 	goto createDaemon
+rem ) 
 
-sc query IoTCenterDaemon |findstr /i "STATE">nul
-if not errorlevel 1 (goto startDaemon) else goto createDaemon
+rem :startDaemon
+rem sc start "IoTCenterDaemon"
 
-:startDaemon
-sc start "IoTCenterDaemon"
+rem :createDaemon
 
-:createDaemon
+rem set "DAEMON_SERVICE_PATH=%~dp0services\daemon"
 
-cd ..\
+cd ..
 
-set DAEMON_SERVICE_PATH=%cd%\services\daemon
-set BIN_SERVICE_PATH=%cd%\bin
+set "DAEMON_SERVICE_PATH=%cd%\services\daemon"
+set "BIN_SERVICE_PATH=%cd%\bin"
+set "WEB_SERVICE_PATH=%cd%\IoTCenterWeb"
 
-sc create IoTCenterDaemon binPath= "\"%DOTNET_PATH%\dotnet.exe\" \"%DAEMON_SERVICE_PATH%\IoTCenterWeb.Daemon.dll\"" DisplayName= "IoTCenterDaemon" start= auto  
-sc start "IoTCenterDaemon"
-echo IoTCenterDaemon Installed Successfully
+cd /d "%dp0%"
 
-@echo off
+rem call :GetTimestamp
+rem echo [!formatted_datetime!] ’˝‘⁄∞≤◊∞IoTCenterDaemon∑˛ŒÒ >> "%logs_dir%"
+
+rem sc create IoTCenterDaemon binPath= "\"%DOTNET_PATH%\dotnet.exe" \"%DAEMON_SERVICE_PATH%\IoTCenterWeb.Daemon.dll"" DisplayName= "IoTCenterDaemon" start= auto  
+rem sc start "IoTCenterDaemon"
+
+rem call :GetTimestamp
+rem echo [!formatted_datetime!] IoTCenterDaemon∑˛ŒÒ∞≤◊∞ÕÍ≥… >> "%logs_dir%"
+
 sc query IoTCenter |findstr /i "STATE">nul
-if not errorlevel 1 (goto exist) else goto notexist 
-
+if not errorlevel 1 (goto exist) else goto notexist
 :exist
 sc start "IoTCenter"
-
-
 :notexist
-sc create IoTCenter binPath= "\"%DOTNET_PATH%\dotnet.exe\" \"%BIN_SERVICE_PATH%\GWHost1.dll\"" DisplayName= "IoTCenter Service" start= auto  
+call :GetTimestamp
+echo [!formatted_datetime!] Installing IoTCenter service. >> "%logs_dir%"
+sc create IoTCenter binPath= "%BIN_SERVICE_PATH%\GWHost1.exe" DisplayName= "IoTCenter Service" start= auto  
 sc start "IoTCenter"
-echo IoTCenter Installed Successfully
+call :GetTimestamp
+echo [!formatted_datetime!] The IoTCenter service installation has been completed. >> "%logs_dir%"
+
+goto Web
+:Web
+sc query IoTCenterWeb |findstr /i "STATE">nul
+if not errorlevel 1 (goto exist) else goto notexist
+:exist
+sc start "IoTCenterWeb"
+pause
+goto end
+:notexist
+call :GetTimestamp
+echo [!formatted_datetime!] Installing IoTCenterWeb service >> "%logs_dir%"
+sc create IoTCenterWeb binPath= "%WEB_SERVICE_PATH%\publish\IoTCenterWebApi.exe" DisplayName= "IoTCenterWeb Service" start= auto
+sc start "IoTCenterWeb"
+call :GetTimestamp
+echo [!formatted_datetime!] The IoTCenterWeb service installation has been completed >> "%logs_dir%"
+
+rem ∆Ù∂ØWeb
+call :GetTimestamp
+echo [!formatted_datetime!] Opening the IoTCenter access link^(https://localhost:44380^)£¨Please Wait... >>%logs_dir%
+rem ªÒ»°ƒ¨»œ‰Ø¿¿∆˜µƒ–≠“Èπÿ¡™£®HTTP£©
+for /f "tokens=2*" %%a in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice" /v ProgId') do set BROWSER=%%b
+rem ≈–∂œ≥£º˚‰Ø¿¿∆˜¿‡–Õ≤¢µ˜”√
+if "%BROWSER%"=="ChromeHTML" (
+    start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --new-window "https://localhost:44380"
+) else if "%BROWSER%"=="MSEdgeHTM" (
+    start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --new-window "https://localhost:44380"
+) else if "%BROWSER%"=="FirefoxURL" (
+    start "" "C:\Program Files\Mozilla Firefox\firefox.exe" -new-window "https://localhost:44380"
+) else if "%BROWSER%"=="AppXq0fevzme2p429mpbidy6na6cqpiv3i5" (
+    call :GetTimestamp
+	echo [!formatted_datetime! Modern versions of Microsoft Edge browser ^(UWP/Win10+ based^) are not supported°£>>%logs_dir%
+) else (
+	call :GetTimestamp
+    echo [!formatted_datetime!] Unrecognized browser type£∫%BROWSER% >>%logs_dir%
+    echo [!formatted_datetime!] Attempting to open using the system default method... >>%logs_dir%
+    start "" "https://localhost:44380"
+)
+
+rem ========== ∫Ø ˝∂®“Â ==========
+:GetTimestamp
+rem π¶ƒ‹£∫ªÒ»°µ±«∞ ±º‰¥¡≤¢¥Ê»Îformatted_datetime±‰¡ø
+rem ∏Ò Ω£∫yyyy-MM-dd HH:mm:ss
+for /f "delims=" %%a in ('powershell -command "Get-Date -Format 'yyyy-MM-dd HH:mm:ss'"') do (
+    set "formatted_datetime=%%a"
+)
+exit /b
 
 pause
--- shells/unregist.bat
+++ shells/unregist.bat
@@ -1,17 +1,67 @@
-echo Start UnInstalled  IoTCenter Service
-echo Current Path %cd%
-cd ..\..\
-echo %cd%
-taskkill /IM dotnet
-set dir=D:\TOMs\IoTCenter\
-echo --------------------
+@echo off
+setlocal enabledelayedexpansion
 
-sc stop IoTCenterDaemon 
-sc delete IoTCenterDaemon 
-echo IoTCenterDaemon UnInstalled Successfully
+title Unregister TOMs Windows Service
+	
+cd /d "%~dp0%"
 
+rem 设置卸载日志目录
+set "logs_dir=%cd%\unregist_logs.txt"
+
+:: 检测管理员权限
+fltmc >nul 2>&1 || (
+	call :GetTimestamp
+    echo [!formatted_datetime!] Please right-click on this batch file and select "Run as administrator."" >>%logs_dir%
+    pause
+    exit /b
+)
+
+if exist "%logs_dir%" (
+	del /q "%logs_dir%"
+	if %errorlevel% equ 0 (
+		call :GetTimestamp
+		echo [!formatted_datetime!] Successfully deleted registry log files: "%logs_dir%" >>%logs_dir%
+    ) else (
+		call :GetTimestamp
+		echo [!formatted_datetime!] Failed to delete registry log files^（files might be locked or you lack proper access rights^） >>%logs_dir%
+    )
+)
+
+rem sc stop IoTCenterDaemon 
+rem sc delete IoTCenterDaemon 
+rem echo IoTCenterDaemon UnInstalled Successfully
+
+sc query IoTCenter |findstr /i "STATE">nul
+if not errorlevel 1 (goto exist) else goto notexist
+ 
+:exist
 sc stop IoTCenter 
-sc delete  IoTCenter
-echo IoTCenter UnInstalled Successfully
+sc delete IoTCenter
+
+call :GetTimestamp
+echo [!formatted_datetime!] IoTCenter UnInstalled Successfully  >>%logs_dir%
+
+:notexist
+
+sc query IoTCenterWeb |findstr /i "STATE">nul
+if not errorlevel 1 (goto exist) else goto notexist
+ 
+:exist
+sc stop IoTCenterWeb 
+sc delete IoTCenterWeb
+
+call :GetTimestamp
+echo [!formatted_datetime!] IoTCenterWeb UnInstalled Successfully  >>%logs_dir%
+
+:notexist
+
+rem ========== 函数定义 ==========
+:GetTimestamp
+rem 功能：获取当前时间戳并存入formatted_datetime变量
+rem 格式：yyyy-MM-dd HH:mm:ss
+for /f "delims=" %%a in ('powershell -command "Get-Date -Format 'yyyy-MM-dd HH:mm:ss'"') do (
+    set "formatted_datetime=%%a"
+)
+exit /b
 
 pause
